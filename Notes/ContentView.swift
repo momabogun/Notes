44a6c8b0aad6446cdbf97411c18858b4c7081840
Momcilo Bogunovic <moma.bogunovic@gmail.com>
2023-12-24 19:08:19 +0100

    Initial Commit

---
+++ Notes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notes
+//
+//  Created by Momcilo Bogunovic on 24.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Momcilo Bogunovic on 24.12.23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
